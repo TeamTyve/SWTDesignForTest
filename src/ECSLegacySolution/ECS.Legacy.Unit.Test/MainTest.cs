df255da5e5239db3662bbb589c3110680f3b8221
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-15 16:35:12 +0100

    MainTest, Had to remove string args

---
+++ src/ECSLegacySolution/ECS.Legacy.Unit.Test/MainTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy.Unit.Test
+{
+    [TestFixture]
+    public class MainTest
+    {
+        [Test]
+        public void Main_CanRun_NoThrow()
+        {
+            var uut = new Application();
+
+            Application.Main();
+        }
+    }
+}
--- src/ECSLegacySolution/ECS.Legacy/Application.cs
+++ src/ECSLegacySolution/ECS.Legacy/Application.cs
@@ -2,7 +2,7 @@
 {
     public class Application
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             var ecs = new ECS(28, new TempSensor(), new Heater());
 
