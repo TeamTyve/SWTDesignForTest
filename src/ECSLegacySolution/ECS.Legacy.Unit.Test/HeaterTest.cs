fd032bb4bfe9645ccf9f14be7f7e1454ff0c49f1
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-15 16:13:19 +0100

    Added Heater Tests

---
+++ src/ECSLegacySolution/ECS.Legacy.Unit.Test/HeaterTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy.Unit.Test
+{
+    [TestFixture]
+    public class HeaterTest
+    {
+        public IHeater Uut { get; set; }
+        [SetUp]
+        public void Init()
+        {
+            Uut = new Heater();
+        }
+
+        [Test]
+        public void Heater_CanTurnOn_NoThrow()
+        {
+            Assert.DoesNotThrow(Uut.TurnOn);
+        }
+
+        [Test]
+        public void Heater_CanTurnOff_NoThrow()
+        {
+            Assert.DoesNotThrow(Uut.TurnOff);
+        }
+
+        [Test]
+        public void Heater_RunSelfTest_ReturnsTrue()
+        {
+            Assert.That(Uut.RunSelfTest, Is.True);
+        }
+    }
+}
